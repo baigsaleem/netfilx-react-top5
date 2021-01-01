6ab4fbd1a56b07bb6610fad357304fc60f49dca0
saleembaig <gojalisaleem1@gmail.com>
2021-01-01 12:27:10 +0500

    made further components of heading and images

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import "./App.css";
+import Heading from "./Heading";
+import Images from "./Images";
 
 function Card(props) {
   return (
     <>
       <div className="cards">
         <div className="card">
-          <img src={props.imgSrc} alt="cover_image" className="card_img" />
+          <Images imgSrc={props.imgSrc} />
           <div className="card_info">
             <span className="card_category">{props.title}</span>
-            <h3 className="card_title">{props.sname}</h3>
+            <Heading sname={props.sname} />
             <a href={props.link} target="_blank">
               <button className="button">Watch Now</button>
             </a>
---
+++ src/Images.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Images = (props) => {
+  return <img src={props.imgSrc} alt="cover_image" className="card_img" />;
+};
+
+export default Images;
